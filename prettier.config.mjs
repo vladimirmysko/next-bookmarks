b0fda0ff25bba6587aba36ff372d1196721387ce
Vladimir Mysko <57433511+vladimirmysko@users.noreply.github.com>
2025-01-03 11:25:40 +0500

    feat: Add PostCSS and Prettier configuration with necessary dependencies

---
+++ prettier.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import("prettier").Options} */
+const config = {
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  useTabs: false,
+  printWidth: 80,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+}
+
+export default config
